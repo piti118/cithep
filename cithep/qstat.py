bc55e0509910b76a57f757ee00859bacfb76bc4f
Piti Ongmongkolkul <piti118@gmail.com>
2012-05-27 11:04:27 -0700

    update

--- cithep/qstat.py
+++ cithep/qstat.py
@@ -1,19 +1,18 @@
 from subprocess import check_output
-from os import getlogin
-class qline:
-	def __init__(self,line):
-		qtoken = line.split()
-		self.jobid = qtoken[0]
-		self.jname = qtoken[1]
-		self.qtime = qtoken[2]
-		self.jstatus = qtoken[3]
-		self.jque = qtoken[4]
-		
-def qstat():
-	qout = check_output('qstat')[1:]
-	ret = []
-	for line in qout:
-		ret.append(qline(line))
-	return ret
-			
-print qstat()
+class Job:
+    def __init__(self,line):
+        qtoken = line.split()
+        self.jobid = qtoken[0]
+        self.jname = qtoken[1]
+        self.owner = qtoken[2]
+        self.qtime = qtoken[3]
+        self.status = qtoken[4]
+        self.jque = qtoken[5]
+    
+    def qstat():
+        qout = check_output('qstat').split('\n')[2:-1]
+        ret = []
+        for line in qout:
+            ret.append(Job(line))
+        return ret
+
